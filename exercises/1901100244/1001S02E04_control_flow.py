365019f320538e9388a4974f5e6bf46a3c02d466
Shen Huang <shen.huang@gmail.com>
2019-09-04 20:16:06 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901100244/1001S02E04_control_flow.py
+++ exercises/1901100244/1001S02E04_control_flow.py
@@ -7,6 +7,8 @@
 
 # 任务原始要求
 
+# 使用 for...in 循环打印九九乘法表
+
 for i in range(1, 10):
     for j in range(1, 10):
         if j <= i:
@@ -14,15 +16,22 @@ for i in range(1, 10):
     print()
 print()
 
-for i in range(1, 10):
-    while i % 2 != 0:
-        for j in range(1, 10):
-            if j <= i:
-                print("{0}*{1}={2}".format(i, j, i*j), end='   ')
-        print()
-        break
+# 使用 while 循环打印九九乘法表并用条件判断把偶数行去掉
+
+a = 1
+while (a < 10):
+    b = 1
+    while b <= a:
+        if a % 2 != 0:
+            print("{}*{}={}".format(a, b, a*b), end='   ')
+            b += 1
+        else:
+            print()
+            break
+    a += 1
 print()
 
+print()
 print("――――――――――――――――――――――――――――――――――――――――")
 print()
 
@@ -48,6 +57,62 @@ for i in range(1, 10):
         else:
             print("{0}×{1}={2}".format(i, j, i*j), end='\t')
     print()
+print()
+
+# 不同的循环方式
+
+for i in range(1, 10):
+    while i % 2 != 0:
+        for j in range(1, 10):
+            if j <= i:
+                print("{0}×{1}={2}".format(i, j, i*j), end='\t')
+        print()
+        break
+print()
+
+# 不同的循环方式，会出现空行
+
+a = 1
+while (a < 10):
+    b = 1
+    while b <= a:
+        if a % 2 != 0:
+            print(a, '×', b, '=', a*b, sep='', end='\t')
+            b += 1
+        else:
+            break
+    a += 1
+    print()
+print()
+
+# 不同的循环方式，避免了空行，逻辑相对更清晰
+
+a = 1
+while (a < 10):
+    b = 1
+    if a % 2 != 0:
+        while b <= a:
+            print(a, '×', b, '=', a*b, sep='', end='\t')
+            b += 1
+        print()
+        a += 1
+    else:
+        a += 1
+print()
+
+# 不同的循环方式，避免了空行，代码相对更简洁
+
+a = 0
+while (a < 10):
+    a += 1
+    b = 1
+    if a % 2 != 0:
+        while b <= a:
+            print(a, '×', b, '=', a*b, sep='', end='\t')
+            b += 1
+        print()
+    else:
+        continue
 print()
 
 # 考虑该表可能的实际用途，去掉了两数大小判断
@@ -98,6 +163,7 @@ print()
 for i in range(1, 10):
     if i % 2 == 0:
         continue
+    else:
         for j in range(1, i+1):
             print("{0}×{1}={2}".format(j, i, i*j), end='\t')
         print()
