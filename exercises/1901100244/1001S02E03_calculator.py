45ad29b95f77d5593c46d871a403cf48046cf3c8
Shen Huang <shen.huang@gmail.com>
2019-09-05 20:37:16 +0800

    Update 1001S02E03_calculator.py

--- exercises/1901100244/1001S02E03_calculator.py
+++ exercises/1901100244/1001S02E03_calculator.py
@@ -5,24 +5,29 @@
 
 # 一个支持 加、减、乘、除功能 的计算器，支持输入参数，支持输出结果
 
+
 def add(x, y):
-    print("{} + {} = ".format(x,y))
+    print("{} + {} = ".format(x, y))
     print(x + y)
 
+
 def sub(x, y):
-    print("{} - {} = ".format(x,y))
+    print("{} - {} = ".format(x, y))
     print(x - y)
 
+
 def mul(x, y):
-    print("{} × {} = ".format(x,y))
+    print("{} × {} = ".format(x, y))
     print(x * y)
+
 
 def div(x, y):
     if y == 0:
         print("输入错误，除数不能为0！")
     else:
-        print("{} ÷ {} = ".format(x,y))
+        print("{} ÷ {} = ".format(x, y))
         print(x / y)
+
 
 x = float(input("请输入第一个数字: "))
 y = float(input("请输入第二个数字: "))
