153fe39c95dd827ca03477b4d49a5cec222e2dc3
Shen Huang <shen.huang@gmail.com>
2019-09-08 18:36:31 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901100244/1001S02E04_control_flow.py
+++ exercises/1901100244/1001S02E04_control_flow.py
@@ -127,10 +127,26 @@ for i in range(1, 10):
     print()
 print()
 
-# 不同的循环方式，使用了列表推导式
+# 不同的循环方式，使用了推导式
 
-print('\n'.join(['\t'.join('%d×%d=%2d' % (x, y, x*y) for x in range(1, y+1))
-      for y in range(1, 10)]))
+for x in [["{0}×{1}={2}".format(j, i, j*i)
+          for j in range(1, 10) if j <= i]
+          for i in range(1, 10)]:
+    for y in x:
+        print(y, end='\t')
+    print()
+
+print('\n'.join(['\t'.join(["{0}×{1}={2}".format(j, i, j*i)
+      for j in range(1, 10) if j <= i]) for i in range(1, 10)]))
+
+print('\n'.join(['\t'.join(["{0}×{1}={2}".format(j, i, j*i)
+      for j in range(1, i+1)]) for i in range(1, 10)]))
+
+print('\n'.join(['\t'.join(['%d×%d=%d' % (j, i, j*i)
+      for j in range(1, i+1)]) for i in range(1, 10)]))
+
+print('\n'.join('\t'.join('%d×%d=%d' % (j, i, j*i)
+      for j in range(1, i+1)) for i in range(1, 10)))
 
 # 不同的循环方式
 
